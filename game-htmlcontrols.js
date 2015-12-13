fdd298fab52f8e59957045925c8c0f17bd7a36cd
RBeaulieu <beaulieu_raph@outlook.com>
2015-12-13 00:09:03 -0400

    Fixed decimal point

--- game-htmlcontrols.js
+++ game-htmlcontrols.js
@@ -4,9 +4,9 @@ function setEyePos(eyeX, eyeY, eyeZ)
 	var myCamY = document.getElementById('myCamY');
 	var myCamZ = document.getElementById('myCamZ');
 	
-	myCamX.innerHTML = (+eyeX.toFixed(2)).toString();
-	myCamY.innerHTML = (+eyeY.toFixed(2)).toString()
-	myCamZ.innerHTML = (+eyeZ.toFixed(2)).toString();
+	myCamX.innerHTML = (eyeX.toFixed(1)).toString();
+	myCamY.innerHTML = (eyeY.toFixed(1)).toString();
+	myCamZ.innerHTML = (eyeZ.toFixed(1)).toString();
 }
 
 function setRefPos(refX, refY, refZ)
@@ -14,7 +14,8 @@ function setRefPos(refX, refY, refZ)
 	var myLookX = document.getElementById('myLookX');
 	var myLookY = document.getElementById('myLookY');
 	var myLookZ = document.getElementById('myLookZ');
-	myLookX.innerHTML = (+refX.toFixed(2)).toString();
-	myLookY.innerHTML = (+refY.toFixed(2)).toString();
-	myLookZ.innerHTML = (+refZ.toFixed(2)).toString();
+	
+	myLookX.innerHTML = (refX.toFixed(1)).toString();
+	myLookY.innerHTML = (refY.toFixed(1)).toString();
+	myLookZ.innerHTML = (refZ.toFixed(1)).toString();
 }
