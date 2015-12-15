292be5e1470c0f7e66635e2e9c64526b9895bb12
elliotcoy <elliotcoy@gmail.com>
2015-12-15 10:53:31 -0400

    LR Strafe Working

--- game-draw.js
+++ game-draw.js
@@ -10,6 +10,8 @@ var g_cubeBuffer = null;
 var g_modelMatrix = new Matrix4(), g_mvpMatrix = new Matrix4();
 // Movement speed
 var g_moveSpeed = 0.5;
+// Look speed
+var g_lookSpeed = 2;
 // Eye coordinates
 var g_eyeX = 13.5, g_eyeY = 13.5, g_eyeZ = 13.5;
 // Reference coordinates
@@ -34,7 +36,9 @@ var u_MVPMatix;
 var controlSet = [];
 
 //CAMERA VARIABLES
+// angle of the xz plane
 var degLR = 0;
+// angle of the yz plane
 var degUD = -90;
 
 function drawInit(currStep)
@@ -272,14 +276,16 @@ function keyUp(ev)
 
 function draw(highResTimestamp) {
 	requestAnimationFrame(draw);
-	//console.time('fps');
+	console.time('fps');
 	
 	// Clear color and depth buffer
 	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
 	VPMatrix.setPerspective(60.0, 600 / 400, 1.0, 200.0);
 	
-	VPMatrix.lookAt(g_eyeX, g_eyeY, g_eyeZ, g_centerX, g_centerY, g_centerZ, 0.0, 1.0, 0.0);
+	VPMatrix.lookAt(g_eyeX,		g_eyeY,		g_eyeZ, 
+					g_centerX,	g_centerY,	g_centerZ, 
+					0.0,		1.0,		0.0);
 	
 	//Read the 3D Array
 	for(var z = 0; z < size; z++)
@@ -296,33 +302,25 @@ function draw(highResTimestamp) {
 			}
 		}
 	}
-	//console.timeEnd('fps');
-
-	if(controlSet[65]){ g_eyeX -= g_moveSpeed; }	// The A key was released
-	if(controlSet[68]){ g_eyeX += g_moveSpeed; }	// The D key was released
-	if(controlSet[69]){ g_eyeY -= g_moveSpeed; }	// The E key was released
-	if(controlSet[81]){ g_eyeY += g_moveSpeed; }	// The Q key was released
-	if(controlSet[83]){ g_eyeZ -= g_moveSpeed; }	// The S key was released
-	if(controlSet[87]){ g_eyeZ += g_moveSpeed; }	// The W key was released
 
 	// The left arrow key was released
 	if(controlSet[37]){
-		degLR -= 2;
+		degLR -= g_lookSpeed;
 	}	
 	// The right arrow key was released
 	if(controlSet[39]){
-		degLR += 2;
+		degLR += g_lookSpeed;
 	}	
 	// The up arrow key was released
 	if(controlSet[38]){
-		if(degUD+2 <= -10){
-			degUD += 2;
+		if(degUD + g_lookSpeed <= -10){
+			degUD += g_lookSpeed;
 		}
 	}	
 	// The down arrow key was released
 	if(controlSet[40]){
-		if(degUD-2 >= -176){
-			degUD -= 2;
+		if(degUD - g_lookSpeed >= -176){
+			degUD -= g_lookSpeed;
 		}
 	}	
 
@@ -330,8 +328,50 @@ function draw(highResTimestamp) {
 	g_centerY = g_eyeY + (10 * Math.cos(degUD*3.1416/180))
 	g_centerZ = g_eyeZ + (10 * Math.sin(degLR*3.1416/180) * Math.sin(degUD*3.1416/180))
 
+	// The A key was released
+	if(controlSet[65]){
+		g_eyeX -= Math.sin(degLR*3.1416/180) * g_moveSpeed;
+		g_eyeZ += Math.cos(degLR*3.1416/180) * g_moveSpeed;
+	}
+	// The D key was released
+	if(controlSet[68]){
+		g_eyeX += Math.sin(degLR*3.1416/180) * g_moveSpeed;
+		g_eyeZ -= Math.cos(degLR*3.1416/180) * g_moveSpeed;
+	}
+
+
+
+	// The E key was released
+	if(controlSet[69]){
+		g_eyeY -= Math.sin(degUD*3.1416/180) * g_moveSpeed;
+		g_eyeY += Math.sin(degUD*3.1416/180) * g_moveSpeed;
+		g_eyeZ += Math.sin(degUD*3.1416/180) * g_moveSpeed;
+	}
+	// The Q key was released
+	if(controlSet[81]){
+		g_eyeY += Math.sin(degUD*3.1416/180) * g_moveSpeed;
+		g_eyeY += Math.sin(degUD*3.1416/180) * g_moveSpeed;
+		g_eyeZ += Math.cos(degLR*3.1416/180) * g_moveSpeed;
+	}
+
+
+	// The S key was released
+	if(controlSet[83]){
+		g_eyeZ += Math.sin(degLR*3.1416/180) * g_moveSpeed;
+	}
+	// The W key was released
+	if(controlSet[87]){
+		g_eyeZ -= Math.sin(degLR*3.1416/180) * g_moveSpeed;
+	}
+
+	g_centerX = g_eyeX + (10 * Math.cos(degLR*3.1416/180) * Math.sin(degUD*3.1416/180))
+	g_centerY = g_eyeY + (10 * Math.cos(degUD*3.1416/180))
+	g_centerZ = g_eyeZ + (10 * Math.sin(degLR*3.1416/180) * Math.sin(degUD*3.1416/180))
+
 	setEyePos(g_eyeX, g_eyeY, g_eyeZ);
 	setRefPos(g_centerX, g_centerY, g_centerZ);
+
+	console.timeEnd('fps');
 }
 
 function drawCube(gl, n, buffer, VPMatrix, a_Position, u_MVPMatrix)
